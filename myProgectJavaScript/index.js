0467cf5fdd21cfd9b2bf30d9f98b86399b4dec11
AdammmVV <adammm.web@gmail.com>
2022-09-30 14:16:05 +0300

    toString

--- myProgectJavaScript/index.js
+++ myProgectJavaScript/index.js
@@ -51,9 +51,23 @@ function whatNumberIsIt(n){
    
  }
 //whatNumberIsIt(1/0)
-whatNumberIsIt(100);
-whatNumberIsIt(1.7976931348623157e+308);
-whatNumberIsIt(-Number.MAX_VALUE*2);
-whatNumberIsIt(5e-324);
-whatNumberIsIt(NaN)
-whatNumberIsIt(Infinity+1)
+//whatNumberIsIt(100);
+//whatNumberIsIt(1.7976931348623157e+308);
+//whatNumberIsIt(-Number.MAX_VALUE*2);
+//whatNumberIsIt(5e-324);
+//whatNumberIsIt(NaN)
+//whatNumberIsIt(Infinity+1)
+
+function colorOf(r,g,b){
+   //coding here
+ 
+   var red = r.toString(16),
+      green = g.toString(16),
+      blue = b.toString(16);
+      if(red.length==1) red = "0"+red;
+      if(green.length==1) green = "0"+green;
+      if(blue.length==1) blue = "0"+blue;
+   console.log("#"+red+green+blue)
+   return "#"+red+green+blue;
+ }
+ colorOf(102,61,8)
