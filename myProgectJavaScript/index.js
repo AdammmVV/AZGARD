0074600f2e3b6ae77095ce7703e5664347029ed3
AdammmVV <adammm.web@gmail.com>
2022-11-04 23:35:52 +0300

    freeCodeCamp

--- myProgectJavaScript/index.js
+++ myProgectJavaScript/index.js
@@ -1464,3 +1464,18 @@ function removeExclamationMarks(s) {
 function stringToArray(string) {
    return string.split(' ');
 }
+
+function zeroArray(m, n) {
+   let newArray = [];
+   for (let i = 0; i < m; i++) {
+      let row = [];
+      for (let j = 0; j < n; j++) {
+         row.push(0);
+      }
+      newArray.push(row);
+   }
+   return newArray;
+}
+
+let matrix = zeroArray(4, 3);
+console.log(matrix);
