132142295a56cc4df8c72861d4db8e457783dd92
AdammmVV <adammm.web@gmail.com>
2022-11-06 14:46:59 +0300

    Array

--- myProgectJavaScript/index.js
+++ myProgectJavaScript/index.js
@@ -1378,6 +1378,250 @@ function htmlColorNames(arr) {
 
 console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
 
+//Копировать элементы массива с помощью slice()
+
+function forecast(arr) {
+   return arr.slice(2, 4);
+}
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+//Скопируйте массив с помощью оператора Spread "([...arr])"
+
+function copyMachine(arr, num) {
+   let newArr = [];
+   while (num >= 1) {
+      newArr.push([...arr])
+      num--;
+   }
+   return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
+
+//Объединение массивов с помощью оператора Spread "([...arr])"
+
+function spreadOut() {
+   let fragment = ['to', 'code'];
+   let sentence = ['learning', ...fragment, 'is', 'fun']
+   return sentence;
+}
+
+console.log(spreadOut());
+
+//Проверить наличие элемента с помощью indexOf()
+
+function quickCheck(arr, elem) {
+   return arr.indexOf(elem) >= 0 ? true : false
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+//Итерация по всем элементам массива с использованием циклов for
+
+function filteredArray(arr, elem) {
+   let newArr = [];
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i].indexOf(elem) < 0) {
+         newArr.push(arr[i])
+      }
+   }
+   return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+
+//Создание сложных многомерных массивов
+
+let myNestedArray = [
+   ['unshift', false, 1, 2, 3, 'complex', 'nested',
+      ['deep',
+         ['deeper',
+            ['deepest'],
+         ],
+      ],
+   ],
+   ['loop', 'shift', 6, 7, 1000, 'method'],
+   ['concat', false, true, 'spread', 'array'],
+   ['mutate', 1327.98, 'splice', 'slice', 'push'],
+   ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+]
+
+//Добавьте пары ключ-значение в объекты JavaScript
+
+let foods = {
+   apples: 25,
+   oranges: 32,
+   plums: 28
+};
+
+foods['bananas'] = 13;
+foods['grapes'] = 35;
+foods['strawberries'] = 27;
+
+console.log(foods);
+
+//Изменить объект, вложенный в объект
+
+let userActivity = {
+   id: 23894201352,
+   date: 'January 1, 2017',
+   data: {
+      totalUsers: 51,
+      online: 42
+   }
+};
+
+userActivity.data.online = 45
+
+console.log(userActivity);
+
+//Доступ к именам свойств с помощью скобочных обозначений
+
+let foods2 = {
+   apples: 25,
+   oranges: 32,
+   plums: 28,
+   bananas: 13,
+   grapes: 35,
+   strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+   return foods[scannedItem]
+}
+
+console.log(checkInventory("apples"));
+
+//Используйте ключевое слово delete для удаления свойств объекта
+
+let foods1 = {
+   apples: 25,
+   oranges: 32,
+   plums: 28,
+   bananas: 13,
+   grapes: 35,
+   strawberries: 27
+};
+
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+
+console.log(foods);
+
+
+let users = {
+   Alan: {
+      age: 27,
+      online: true
+   },
+   Jeff: {
+      age: 32,
+      online: true
+   },
+   Sarah: {
+      age: 48,
+      online: true
+   },
+   Ryan: {
+      age: 19,
+      online: true
+   }
+};
+
+function isEveryoneHere(userObj) {
+   return userObj.hasOwnProperty('Alan') &&
+      userObj.hasOwnProperty('Jeff') &&
+      userObj.hasOwnProperty('Sarah') &&
+      userObj.hasOwnProperty('Ryan');
+}
+
+console.log(isEveryoneHere(users));
+
+//Итерация по ключам объекта с оператором for...in
+
+const usersobj = {
+   Alan: {
+      online: false
+   },
+   Jeff: {
+      online: true
+   },
+   Sarah: {
+      online: false
+   }
+}
+
+function countOnline(usersObj) {
+   let res = 0;
+   for (let user in usersObj) {
+      console.log(usersObj[user])
+      if (usersObj[user]['online']) {
+         res = res + 1;
+      }
+   }
+   return res
+}
+
+console.log(countOnline(usersobj));
+
+//Создайте массив всех ключей объекта с помощью Object.keys()
+
+let users123 = {
+   Alan: {
+     age: 27,
+     online: false
+   },
+   Jeff: {
+     age: 32,
+     online: true
+   },
+   Sarah: {
+     age: 48,
+     online: false
+   },
+   Ryan: {
+     age: 19,
+     online: true
+   }
+ };
+ 
+ function getArrayOfUsers(obj) {
+   return Object.keys(obj)
+ }
+ 
+ console.log(getArrayOfUsers(users123));
+
+// Изменить массив, хранящийся в объекте
+
+ let user = {
+   name: 'Kenneth',
+   age: 28,
+   data: {
+     username: 'kennethCodesAllDay',
+     joinDate: 'March 26, 2016',
+     organization: 'freeCodeCamp',
+     friends: [
+       'Sam',
+       'Kira',
+       'Tomo'
+     ],
+     location: {
+       city: 'San Francisco',
+       state: 'CA',
+       country: 'USA'
+     }
+   }
+ };
+ 
+ function addFriend(userObj, friend) {
+   userObj.data.friends.push(friend)
+  return userObj.data.friends
+ }
+ 
+ console.log(addFriend(user, 'Pete'));
+
+
 // КАЛЬКУЛЯТОР
 
 function basicOp(operation, value1, value2) {
@@ -1568,3 +1812,9 @@ function contamination1(text, char) {
 }
 
 console.log(contamination("abc", "z"))
+
+//поиск 'r' при помощи регулярного выражения
+
+function areYouPlayingBanjo(name) {
+   return /^[r]/i.test(name) ? `${name} plays banjo` : `${name} does not play banjo`;
+}
