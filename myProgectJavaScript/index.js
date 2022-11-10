ea9d7ad4425288fd5c26e0605cb99818b5b6a7a0
AdammmVV <adammm.web@gmail.com>
2022-11-10 20:13:03 +0300

    freeCodeCamp

--- myProgectJavaScript/index.js
+++ myProgectJavaScript/index.js
@@ -1949,6 +1949,45 @@ let ifunModule = (function () {
    }
 })();
 
+//==========Функциональное===============
+//программирование Понимание опасностей использования императивного кода
+// tabs is an array of titles of each site open within the window
+const Window = function (tabs) {
+   this.tabs = tabs; // We keep a record of the array inside the object
+};
+
+// When you join two windows into one window
+Window.prototype.join = function (otherWindow) {
+   this.tabs = this.tabs.concat(otherWindow.tabs);
+   return this;
+};
+
+// When you open a new tab at the end
+Window.prototype.tabOpen = function (tab) {
+   this.tabs.push('new tab'); // Let's open a new tab for now
+   return this;
+};
+
+Window.prototype.tabClose = function (index) {
+   this.tabs.splice(index, 1);
+   return this;
+};
+
+// Let's create three browser windows
+const workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+const socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+const videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); // Entertainment sites
+
+// Now perform the tab opening, closing, and other operations
+const finalTabs = socialWindow
+   .tabOpen() // Open a new tab for cat memes
+   .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+   .join(workWindow.tabClose(1).tabOpen());
+console.log(finalTabs.tabs);
+//==============================================================================
+
+
+
 // КАЛЬКУЛЯТОР
 
 function basicOp(operation, value1, value2) {
