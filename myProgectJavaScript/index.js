3cf4b625349b92ed2166a64f7d5d55fe15b53b51
AdammmVV <adammm.web@gmail.com>
2022-11-13 22:38:33 +0300

    CodeWars

--- myProgectJavaScript/index.js
+++ myProgectJavaScript/index.js
@@ -2718,3 +2718,18 @@ console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim"
 function sumStr(a, b) {
    return (+a + +b).toString();
 }
+
+//расчёт BMI
+
+function bmi(weight, height) {
+   let bmi = weight / (height ** 2)
+   if (bmi <= 18.5) {
+      return "Underweight";
+   } else if (bmi <= 25.0) {
+      return "Normal";
+   } else if (bmi <= 30.0) {
+      return "Overweight";
+   } else {
+      return "Obese";
+   }
+}
