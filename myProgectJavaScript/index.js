41386d51c1ed7a93616e3498d06e5ad674b93544
AdammmVV <adammm.web@gmail.com>
2022-11-17 12:18:09 +0300

    fix

--- myProgectJavaScript/index.js
+++ myProgectJavaScript/index.js
@@ -2843,4 +2843,19 @@ function disemvowel(str) {
 
 function addFive(num) {
    return num + 5
- }
+}
+
+String.prototype.camelCase = function () {
+   const arr = this.split(' ')
+   const arrRes = []
+   for (let i = 0; i < arr.length; i++) {
+      arrRes.push(arr[i].replace(/./, el => el.toUpperCase()))
+   }
+   return arrRes.join('')
+}
+
+function stringParse(string) {
+   return string.replace(/.+/g, el => [el])
+}
+
+console.log(stringParse("aaaabbcdefffffffg"))
