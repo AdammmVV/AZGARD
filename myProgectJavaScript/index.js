7a60f871d44f4c62a51b76f8d302a99d831bf9a6
AdammmVV <adammm.web@gmail.com>
2022-11-17 17:43:22 +0300

    CodeWars

--- myProgectJavaScript/index.js
+++ myProgectJavaScript/index.js
@@ -2859,3 +2859,9 @@ function stringParse(string) {
 }
 
 console.log(stringParse("aaaabbcdefffffffg"))
+
+function hoopCount(n) {
+   return n >= 10 ?
+     "Great, now move on to tricks" :
+     "Keep at it until you get it";
+ }
