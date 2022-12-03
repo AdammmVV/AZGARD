a6c3f9a241ea8f4bd26be59c3073f508ebe884f7
AdammmVV <adammm.web@gmail.com>
2022-12-03 20:47:51 +0300

    react-Dialogs

--- myProjectReact/01-first-project/react-project-1/src/App.js
+++ myProjectReact/01-first-project/react-project-1/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-
+import React from "react";
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
-import Content from './components/Profile/Content'; 
+import Content from './components/Profile/Content';
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
-   return (
-      <div className="app-wrapper">
-         <Header />
-         <Nav />
-         <Content />
-      </div>
-   );
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Nav/>
+            <div className='app-wrapper-content'>
+                <Dialogs/>
+                {/*<Content />*/}
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
--- myProjectReact/01-first-project/react-project-1/src/components/Profile/Content.jsx
+++ myProjectReact/01-first-project/react-project-1/src/components/Profile/Content.jsx
@@ -3,12 +3,12 @@ import MyPost from './MyPost/MyPost';
 
 const Content = () => {
    return (
-      <div className={s.content}>
+      <div>
          <div className={s.contentImg}>
             <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-after/Landscape-BW.jpg" />
          </div>
          <div className={s.avatar}>
-            ava + descrition
+            ava + description
             <img src="https://thumbs.dreamstime.com/b/cosmos-beauty-deep-space-elements-image-furnished-nasa-science-fiction-art-102581846.jpg" />
          </div>
          <MyPost/>
