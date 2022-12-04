95915a68151bed20eb6114ac0ec25ade0b651ec8
AdammmVV <adammm.web@gmail.com>
2022-12-04 18:25:31 +0300

    codeWars

--- myProjectJavaScript/index.js
+++ myProjectJavaScript/index.js
@@ -3657,3 +3657,21 @@ function sumMul(n, m) {
 function test() {
    console.log('test');
 }
+
+function evalObject(value){
+   var result = 0;
+   switch(value.operation){
+      case'+': result = value.a + value.b
+         break;
+      case'-': result = value.a - value.b
+         break;
+      case'/': result = value.a / value.b
+         break;
+      case'*': result = value.a * value.b
+         break;
+      case'%': result = value.a % value.b
+         break;
+      case'^': result = Math.pow(value.a, value.b);
+   }
+   return result;
+}
