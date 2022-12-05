4dbc064a0ac876de9846115690ea3a2709b41e0b
AdammmVV <adammm.web@gmail.com>
2022-12-06 01:06:11 +0300

    reactPosts

--- myProjectReact/01-first-project/react-project-1/src/components/Profile/MyPost/MyPosts.jsx
+++ myProjectReact/01-first-project/react-project-1/src/components/Profile/MyPost/MyPosts.jsx
@@ -3,15 +3,17 @@ import Post from './Post/Post';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const MyPosts = () => {
-    let postData = [
+    let postsData = [
         {id: '1', message: "Hi, how are you?", likesCount: '20'},
         {id: '2', message: "It's my first post", likesCount: '15'},
-    ]
+    ];
+
+    let posts = postsData.map(p => <Post message={p.message} amountLike={p.likesCount}/>)
+
    return (
       <div>
          <ProfileInfo/>
-         <Post message={postData[0].message} amountLike={postData[0].likesCount} />
-         <Post message={postData[1].message} amountLike={postData[1].likesCount} />
+          { posts }
       </div>
    );
 }
