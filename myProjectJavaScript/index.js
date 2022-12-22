bd18e126d31a986dc4a07ea531fc30d212264996
AdammmVV <adammm.web@gmail.com>
2022-12-22 22:46:02 +0300

    CodeWars

--- myProjectJavaScript/index.js
+++ myProjectJavaScript/index.js
@@ -3857,3 +3857,17 @@ function nthEven(n) {
 function mango(quantity, price) {
     return (quantity - Math.floor(quantity / 3)) * price
 }
+
+var hotpo = function (n) {
+    if (n == 0) return 0;
+    let i = 0
+    while (n > 1) {
+        if (n % 2 !== 0) {
+            n = 3 * n + 1;
+        } else if (n % 2 == 0) {
+            n /= 2;
+        }
+        i++
+    }
+    return i;
+}
